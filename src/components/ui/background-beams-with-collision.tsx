69b6871ca7891376f96d36364def21f5c69289b5
AndresBarretoDev <andres.barretos24@gmail.com>
2025-07-24 17:03:30 -0500

    fix: update height in BackgroundBeamsWithCollision component
    - Changed height from 99vh to 100dvh for improved responsiveness and layout consistency.

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
         <div
             ref={parentRef}
             className={cn(
-                "h-[99vh] relative flex items-center w-full justify-center overflow-hidden",
+                "h-[100dvh] relative flex items-center w-full justify-center overflow-hidden",
                 // h-screen if you want bigger
                 className
             )}
